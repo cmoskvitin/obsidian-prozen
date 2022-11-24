7cf7982a9530410f519ece11ae064b58879fac3e
Constantin <constantinmoskvitin@gmail.com>
2022-11-24 13:24:35 +0400

    Added radial gradient, cleaned up settings

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ ProZen not just simply turns your active tab fullscreen - it removes every singl
 
 ## Usage
 ### Manual Installation
+Hopefully, this plugin will make it to [Obsidian's plugin gallery](https://obsidian.md/plugins), until then, you can install it manually:
 1. Download the `main.js`, `styles.css` and `manifest.json` files.
 2. Go to the `/.obsidian/plugins` folder of your Obsidian vault. Create `/obsidian-prozen` in there.
 3. Put the downloaded files into the `/obsidian-prozen` folder.
@@ -20,23 +21,24 @@ ProZen not just simply turns your active tab fullscreen - it removes every singl
 ### Plugin Settings
 #### Vignette
 Dims the sides of the screen gradually, drawing your attention to the text in the middle. Especially effective in the dark room with a dark Obsidian theme. The gradient starts from full transparent to black from the text to the sides. 
-- **Vignette opacity**: makes the dimming effect softer or darker. 0 turns off the vignette.
-- **Vignette scale**: determines how much of the screen space is dimmed.
+- **Opacity**: makes the dimming effect softer or darker. 0 turns off the vignette.
+- **Scale**: determines how much of the screen space is dimmed.
+#### Animations
+- **Fade-in duration**: the duration of the content fade-in animation when entering Zen mode. Just my personal thing. With this animation (I set it to two seconds), the document draws more of my attention and sets me to a productive and inspired mood. 0 turns off the animation.
 #### Toggles
 Turn off some screen elements
 - **Show header**: enables/disables the tab's header in Zen mode. Note that vignette is not applied to the header.
 - **Show scrollbar**: enables/disables the vertical scrollbar (visually, scrolling is still available).
-#### Animations
-- **Fade-in duration**: the duration of the content fade-in animation when entering Zen mode. Just my personal thing. With this animation (I set it to two seconds), the document draws more of my attention and sets me to a productive and inspired mood. 0 turns off the animation.
+
 
 ## Known Issues
-- ~~Toggle on/off Zen mode **using your custom hotkey**. **Don't press Esc** to exit fullscreen, or the styles (vignette and show/hide toggles) will still apply to the content.~~ Fixed by [https://github.com/Quorafind](Quorafind), will be included in the next release.
+- ...
 
 ## About me
 My name (last name) is Moskvitin, I am an amateur self-taught JS developer, taking coding as a hobby. I hope, someday, I will be skilled enough to turn this hobby into a full-time job. I realize that experienced developers may find my code sloppy and sub-optimal, but I take my pet projects seriously, hunt for perfection in tiny things and try my best to fix the issues I can fix being at this skill level.
 IRL I'm a tech writer for a company, which develops some sophisticated stuff, and describing this stuff requires full concentration on fine wording. So, ProZen is what I made for myself to use on daily basis.
 
-I really appreciate any feedback and feature requests on GitHub. You can also...
+I really appreciate any [feedback and feature requests](https://github.com/cmoskvitin/obsidian-prozen/issues) on GitHub. You can also...
 [<img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=moskvitin&button_colour=FF5F5F&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" alt="BuyMeACoffee" width="200">](https://www.buymeacoffee.com/moskvitin)
 :)
 
--- main.ts
+++ main.ts
@@ -5,15 +5,17 @@ interface PluginSettings {
 	showHeader: boolean,
 	showScroll: boolean,
 	vignetteOpacity: number,
-	vignetteScale: number
+	vignetteScaleLinear: number,
+	vignetteScaleRadial: number
 }
 
 const DEFAULT_SETTINGS: PluginSettings = {
-	animationDuration: 1.2,
+	animationDuration: 2,
 	showHeader: false,
 	showScroll: true,
-	vignetteOpacity: 0.5,
-	vignetteScale: 20
+	vignetteOpacity: 0.75,
+	vignetteScaleLinear: 20,
+	vignetteScaleRadial: 75
 }
 
 export default class Prozen extends Plugin {
@@ -49,9 +51,10 @@ export default class Prozen extends Plugin {
 		const containerEl = leaf.containerEl;
 		const viewEl = leaf.view.contentEl
 		const root = document.documentElement
-				root.style.setProperty('--vignette-opacity', this.settings.vignetteOpacity)
 				root.style.setProperty('--fadeIn-duration', this.settings.animationDuration + 's')
-				root.style.setProperty('--vignette-scale', this.settings.vignetteScale + '%')
+				root.style.setProperty('--vignette-opacity', this.settings.vignetteOpacity)
+				root.style.setProperty('--vignette-scale-linear', this.settings.vignetteScaleLinear + '%')
+				root.style.setProperty('--vignette-scale-radial', this.settings.vignetteScaleRadial + '%')
 		const header = leaf.view.headerEl
 
 		if (!document.fullscreenElement){
@@ -62,18 +65,18 @@ export default class Prozen extends Plugin {
 			}
 
 			viewEl.classList.add("animate")
-			leaf.view.getViewType() === "graph" ? viewEl.classList.add("vignette-radial") : viewEl.classList.add("vignette")
+			leaf.view.getViewType() === "graph" ? viewEl.classList.add("vignette-radial") : viewEl.classList.add("vignette-linear")
 			this.settings.showHeader ? header.classList.add("animate") : header.classList.add("hide")
 		} else {
 			document.exitFullscreen();
 
-			viewEl.classList.remove("vignette", "vignette-radial", "animate", "noscroll")
+			viewEl.classList.remove("vignette-linear", "vignette-radial", "animate", "noscroll")
 			header.classList.remove("animate", "hide")
 		}
 
 		containerEl.onfullscreenchange = () => {
-			if (!document.fullscreenElement && (viewEl.classList.contains("vignette") || viewEl.classList.contains("vignette-radial"))){
-				viewEl.classList.remove("vignette", "vignette-radial", "animate", "noscroll");
+			if (!document.fullscreenElement && (viewEl.classList.contains("vignette-linear") || viewEl.classList.contains("vignette-radial"))){
+				viewEl.classList.remove("vignette-linear", "vignette-radial", "animate", "noscroll");
 				header.classList.remove("animate", "hide");
 			}
 		}
@@ -93,46 +96,72 @@ class ProzenSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
+		this.containerEl.createEl("h3", {
+			text: "Vignette",
+		})
+
 // VIGNETTE OPACITY SETTING
-		let opacityLevel: HTMLDivElement;
+		let vignetteOpacityNumber: HTMLDivElement;
 		new Setting(containerEl)
-			.setName('Vignette opacity')
-			.setDesc("Opacity of the vignette's initial color. The higher the value the more saturated the color is. Set to 0 to turn vignetting off.")
+			.setName('Opacity')
+			.setDesc("Intensity of vignette's dimming effect. Set to 0 to turn vignetting off.")
 			.addSlider((slider) => slider
 				.setLimits(0.00,1,0.01)
 				.setValue(this.plugin.settings.vignetteOpacity)
 				.onChange(async (value) => {
-					opacityLevel.innerText = " " + value.toString();
+					vignetteOpacityNumber.innerText = " " + value.toString();
 					this.plugin.settings.vignetteOpacity = value;
 					await this.plugin.saveSettings();
 				}))
 				.settingEl.createDiv("", (el: HTMLDivElement) => {
-					opacityLevel = el;
+					vignetteOpacityNumber = el;
 					el.style.minWidth = "2.0em";
 					el.style.textAlign = "right";
 					el.innerText = " " + this.plugin.settings.vignetteOpacity.toString();
 				});
 
-// VIGNETTE SCALE SETTING
-		let vignetteScaleNumber: HTMLDivElement;
+// VIGNETTE SCALE LINEAR SETTING
+		let vignetteScaleLinearNumber: HTMLDivElement;
 		new Setting(containerEl)
-			.setName('Vignette scale')
-			.setDesc("How far the vignetting spreads. The higher the value the closer it is to the middle.")
+			.setName('Scale in text views')
+			.setDesc("Determines how close to the screen's center vignetting spreads from both sides of the screen, as linear gradients.")
 			.addSlider((slider) => slider
-				.setLimits(5,30,5)
-				.setValue(this.plugin.settings.vignetteScale)
+				.setLimits(5,50,5)
+				.setValue(this.plugin.settings.vignetteScaleLinear)
 				.onChange(async (value) => {
-					vignetteScaleNumber.innerText = " " + value.toString();
-					this.plugin.settings.vignetteScale = value;
+					vignetteScaleLinearNumber.innerText = " " + value.toString();
+					this.plugin.settings.vignetteScaleLinear = value;
 					await this.plugin.saveSettings();
 				}))
 				.settingEl.createDiv("", (el: HTMLDivElement) => {
-					vignetteScaleNumber = el;
+					vignetteScaleLinearNumber = el;
 					el.style.minWidth = "2.0em";
 					el.style.textAlign = "right";
-					el.innerText = " " + this.plugin.settings.vignetteScale.toString();
+					el.innerText = " " + this.plugin.settings.vignetteScaleLinear.toString();
+				});
+// VIGNETTE SCALE RADIAL SETTING
+		let vignetteScaleRadialNumber: HTMLDivElement;
+		new Setting(containerEl)
+			.setName('Scale in graph view')
+			.setDesc("Determines how close to the screen's center vignetting spreads from borders of the screen, as a radial gradient.")
+			.addSlider((slider) => slider
+				.setLimits(5,100,5)
+				.setValue(this.plugin.settings.vignetteScaleRadial)
+				.onChange(async (value) => {
+					vignetteScaleRadialNumber.innerText = " " + value.toString();
+					this.plugin.settings.vignetteScaleRadial = value;
+					await this.plugin.saveSettings();
+				}))
+				.settingEl.createDiv("", (el: HTMLDivElement) => {
+					vignetteScaleRadialNumber = el;
+					el.style.minWidth = "2.0em";
+					el.style.textAlign = "right";
+					el.innerText = " " + this.plugin.settings.vignetteScaleRadial.toString();
 				});
 
+		this.containerEl.createEl("h3", {
+			text: "Animation",
+		})
 // CONTENT FADE-IN DURATION SETTING
 		new Setting(containerEl)
 			.setName('Fade-in duration')
@@ -141,9 +170,13 @@ class ProzenSettingTab extends PluginSettingTab {
 				.setPlaceholder('1.2')
 				.setValue(String(this.plugin.settings.animationDuration))
 				.onChange(async (value) => {
-					this.plugin.settings.animationDuration = Number(value);
+					this.plugin.settings.animationDuration = Number(value)
 					await this.plugin.saveSettings();
 				}));
+
+		this.containerEl.createEl("h3", {
+			text: "Element Toggles",
+		})
 
 // SHOW HEADER TOGGLE SETTING
 		new Setting(containerEl)
